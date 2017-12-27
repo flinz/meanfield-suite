4cb3ddfed511038ed4b8bf33836704dd937e4ef5
Teo Stocco <teo@zifeo.com>
2017-12-27 14:06:01 +0100

    attempt gc fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 from time import sleep
 
 import pytest
-from brian2 import device
+from brian2 import device, gc
 
 
 @pytest.yield_fixture(autouse=True)
@@ -9,6 +9,7 @@ def run_around_tests():
     yield
     device.reinit()
     device.activate()
+    gc.collect()
     # workaround for cpp generation errors when running multiple tests in a row (reinit might take some time?)
     sleep(2)
 
